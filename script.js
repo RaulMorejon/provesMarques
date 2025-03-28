d985dd2a80f7267b6e1d37015621c3e291edf31e
Raúl Morejón <raulmorejon@iesebre.com>
2025-03-28 14:11:15 +0100

    prova

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ function cargarYMostrarJSON(url) {
       const container = document.getElementById('json-container');
       const pre = document.createElement('pre');
 
+
       pre.textContent = JSON.stringify(data[0].versio.variables, null, 2).replace(/[\{\}]/g, '');
+      pre.textContent = JSON.stringify(data[1].versio.variables, null, 2).replace(/[\{\}]/g, '');
+      pre.textContent = JSON.stringify(data[2].versio.variables, null, 2).replace(/[\{\}]/g, '');
       container.innerHTML = ''; 
       container.appendChild(pre);
     
-      console.log(data[0].versio.variables);
+      
   
     }) 
     .catch(error => {
